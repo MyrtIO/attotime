65db15d7cb22257e6d140a4abc90b56b65c765cb
mishamyrt <misha@myrt.co>
2025-01-07 01:22:15 +0300

    feat: add copy handler, started state and reset

--- src/progress.h
+++ src/progress.h
@@ -7,44 +7,78 @@
  */
 class Progress {
 public:
-  /**
-   * Returns the current progress as a value between 0 and max_.
-   */
-  uint8_t get() {
-    now_ = millis();
-    if (now_ >= end_) {
-      return max_;
-    }
-    return map(now_, start_, end_, 0, max_);
-  }
+	Progress() {}
+	~Progress() {}
 
-  /**
-   * Starts a progress timer with the given duration in milliseconds.
-   * @param ms The duration of the progress timer in milliseconds.
-   */
-  void start(size_t ms) {
-    start_ = millis();
-    end_   = start_ + ms;
-  }
+	Progress& operator =(const Progress& other) {
+		start_ = other.start_;
+		end_   = other.end_;
+		max_   = other.max_;
+		return *this;
+	}
 
-  /**
-   * Sets the maximum value of the progress timer.
-   * @param max The maximum value of the progress timer.
-   */
-  void setMax(uint8_t max) {
-    max_ = max;
-  }
+	bool operator==(const Progress& rhs) {
+		return start_ == rhs.start_ && end_ == rhs.end_ && max_ == rhs.max_;
+	}
+	bool operator!=(const Progress& rhs) {
+		return start_ != rhs.start_ || end_ != rhs.end_ || max_ != rhs.max_;
+	}
 
-  /**
-   * Returns true if the progress timer has finished.
-   */
-  bool finished() {
-    return millis() >= end_;
-  }
+	/**
+	 * Returns true if the progress timer has been started.
+	 */
+	bool started() {
+		return start_ != 0;
+	}
+
+	/**
+	 * Returns true if the progress timer has finished.
+	 */
+	bool finished() {
+		return started() && millis() >= end_;
+	}
+
+	/**
+	 * Returns the current progress as a value between 0 and max_.
+	 */
+	inline uint8_t get() {
+		if (!started()) {
+			return 0;
+		}
+    	uint32_t now = millis();
+		if (now >= end_) {
+			return max_;
+		}
+		return map(now, start_, end_, 0, max_);
+	}
+
+	/**
+	 * Starts a progress timer with the given duration in milliseconds.
+	 * @param ms The duration of the progress timer in milliseconds.
+	 */
+	inline void start(size_t ms) {
+		start_ = millis();
+		end_   = start_ + ms;
+	}
+
+	/**
+	 * Sets the maximum value of the progress timer.
+	 * @param max The maximum value of the progress timer.
+	 */
+	void setMax(const uint8_t max) {
+		max_ = max;
+	}
+
+	/**
+	 * Resets the progress timer.
+	 */
+	void reset() {
+		start_ = 0;
+		end_   = 0;
+	}
 
 private:
-  uint32_t start_ = 0;
-  uint32_t end_   = 0;
-  uint32_t now_   = 0;
-  uint8_t max_    = 255;
+	uint32_t start_ = 0;
+	uint32_t end_   = 0;
+	uint8_t max_    = 255;
 };
