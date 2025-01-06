80e52251dbf79d9159ca69ebd892b7e8b508f5dc
mishamyrt <misha@myrt.co>
2025-01-07 01:22:43 +0300

    feat: add timer assign handler

--- src/timer.h
+++ src/timer.h
@@ -7,21 +7,50 @@
  */
 class Timer {
 public:
-  /**
-   * Returns true if the timer has finished.
-   */
-  bool finished() {
-    return millis() >= end_;
-  }
+	Timer() {}
+	~Timer() {}
 
-  /**
-   * Starts the timer with the given duration in milliseconds.
-   * @param ms The duration of the timer in milliseconds.
-   */
-  void start(size_t ms) {
-    end_ = millis() + ms;
-  }
+	Timer& operator =(const Timer& other) {
+		end_ = other.end_;
+		return *this;
+	}
+
+	bool operator==(const Timer& rhs) {
+		return end_ == rhs.end_;
+	}
+	bool operator!=(const Timer& rhs) {
+		return end_ != rhs.end_;
+	}
+
+	/**
+	 * Returns true if the timer has finished.
+	 */
+	bool finished() {
+		return millis() >= end_;
+	}
+
+	/**
+	 * Returns true if the timer has been started.
+	 */
+	bool started() {
+		return end_ != 0;
+	}
+
+	/**
+	 * Resets the timer.
+	 */
+	void reset() {
+		end_ = 0;
+	}
+
+	/**
+	 * Starts the timer with the given duration in milliseconds.
+	 * @param ms The duration of the timer in milliseconds.
+	 */
+	void start(size_t ms) {
+		end_ = millis() + ms;
+	}
 
 private:
-  uint32_t end_ = 0;
+	uint32_t end_ = 0;
 };
